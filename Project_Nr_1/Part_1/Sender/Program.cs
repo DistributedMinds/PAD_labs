245759d546e5d4bada6e835eab563b9f12b95de9
Victor <victor.borta10@gmail.com>
2026-09-19 20:03:48 +0300

    Added XML Support

--- Project_Nr_1/Part_1/Sender/Program.cs
+++ Project_Nr_1/Part_1/Sender/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Xml.Serialization;
 using Message_Agent.Common;
 using Newtonsoft.Json;
 using Sender;
@@ -13,15 +14,38 @@ if (senderSocket.IsConected)
     while (true)
     {
         var payLoad = new PayLoad();
-        
-        Console.WriteLine("Enter topic:");
+
+        Console.Write("Enter topic: ");
         payLoad.Topic = Console.ReadLine().ToLower();
-        Console.WriteLine("Enter message:");
+        Console.Write("Enter message: ");
         payLoad.Message = Console.ReadLine();
+        var formatChoice = string.Empty;
+
+        do
+        {
+            Console.Write("Choose serialization format (1 for JSON, 2 for XML): ");
+            formatChoice = Console.ReadLine();
+            if (formatChoice != "1" && formatChoice != "2")
+            {
+                Console.WriteLine("Invalid choice. Choose again");
+            }
+        } while (formatChoice != "1" && formatChoice != "2");
+
+        string payLoadString;
+
+        if (formatChoice == "1")
+        {
+            payLoadString = JsonConvert.SerializeObject(payLoad);
+        }
+        else
+        {
+            var serializer = new XmlSerializer(typeof(PayLoad));
+            using var writer = new StringWriter();
+            serializer.Serialize(writer, payLoad);
+            payLoadString = writer.ToString();
+        }
         
-        var payLoadString = JsonConvert.SerializeObject(payLoad);
         byte[] data = Encoding.UTF8.GetBytes(payLoadString);
-        
         senderSocket.Send(data);
     }
 }
